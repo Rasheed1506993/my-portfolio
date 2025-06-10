164d1c1eea7b00d0543dfda6ef4ba04be2dd5f97
Rasheed1506993 <rashed734002725@gmail.com>
2025-06-10 15:32:36 +0000

    update vite code

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [
     react(),
   ],
   base: '/my-portfolio',
-}));
+});
 
